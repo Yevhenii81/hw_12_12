aae1192d7139495ca88d0f9feb33d66e8a50060e
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2023-12-17 16:15:07 +0200

    Add files via upload

---
+++ hw_12_12/hw_12_12.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+class Counter {
+    int now = 0;
+    int min = 0;
+    int max = 9999;
+    int increment = 1;
+
+public:
+    void SetNow(int n)
+    {
+        now = n;
+    }
+
+    int GetNow() const 
+    {
+        return now;
+    }
+
+    void SetMin(int mi) 
+    {
+        min = mi;
+    }
+
+    int GetMin() const
+    {
+        return min;
+    }
+
+    void SetMax(int ma)
+    {
+        max = ma;
+    }
+
+    int GetMax() const
+    {
+        return max;
+    }
+
+    void SetPlus(int i)
+    {
+        increment = i;
+    }
+
+    int GetPlus() const
+    {
+        return increment;
+    }
+
+    void Realization()
+    {
+        while (true) {
+
+            if (now == max) {
+                now = min;
+                Sleep(5000);
+            }
+            now += increment;
+            cout << now << "\n";
+
+            if (now >= max) now = min;
+        }
+    }
+};
+
+int main()
+{
+    Counter c;
+    c.Realization();
+}
+
